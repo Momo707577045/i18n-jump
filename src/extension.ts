c69111ffd9387cf09a8334e5625275e2ec9ee458
jingwen.mao <jingwen.mao@mintegral.com>
2023-03-29 18:24:10 +0800

    feat: store jump 功能开发，完善 readme

--- src/extension.ts
+++ src/extension.ts
@@ -17,11 +17,7 @@ import {
 } from "vscode";
 
 // 获取当前参数的全层级路径，
-function getParamPaths(
-  document: TextDocument,
-  line: TextLine,
-  firstWord: string
-) {
+function getParamPaths(document: TextDocument, line: TextLine, firstWord: string) {
   let stackNum = 0;
   const namePath = [firstWord]; // 路径
   for (let index = line.lineNumber - 1; index > 0; index--) {
@@ -57,14 +53,7 @@ function getParamPosition(fileStr: string, originParamPaths: string[]) {
         regexp = new RegExp(`\\W${paramPaths[0]}\\W`);
       }
     }
-    console.log(
-      "getParamPosition",
-      currentLine,
-      lastWord,
-      paramPaths,
-      originParamPaths,
-      fileLines[currentLine]
-    );
+    console.log("getParamPosition", currentLine, lastWord, paramPaths, originParamPaths, fileLines[currentLine]);
     // 还有路径没匹配完，证明未命中
     if (paramPaths.length) {
       return false;
@@ -88,9 +77,7 @@ function switchTsI18n(document: TextDocument, position: Position): any {
   }
 
   const isZh = fileName.includes("zh-CN"); // 当前是否为中文字符串
-  const targetFilePath = isZh
-    ? fileName.replace("zh-CN", "en-US")
-    : fileName.replace("en-US", "zh-CN");
+  const targetFilePath = isZh ? fileName.replace("zh-CN", "en-US") : fileName.replace("en-US", "zh-CN");
 
   // 对应翻译文件不存在
   if (!fs.existsSync(targetFilePath)) {
@@ -135,11 +122,7 @@ function switchJsonI18n(document: TextDocument, position: Position): any {
 }
 
 // 针对跳转至具体的翻译
-function jumpI18n(
-  lang: "en" | "cn",
-  textEditor: TextEditor,
-  edit: TextEditorEdit
-): any {
+function jumpI18n(lang: "en" | "cn", textEditor: TextEditor, edit: TextEditorEdit): any {
   const { document } = textEditor;
   const fileName = document.fileName; // 当前文件完整路径
   const fileStr = fs.readFileSync(fileName, "utf-8") as string; // 文件文本
@@ -148,10 +131,7 @@ function jumpI18n(
 
   // 存在翻译特别注入的翻译文件，则先从这里找，否则再从全局找
   if (customI18nPath) {
-    const tempNamePath = [
-      lang === "en" ? `"en-US"` : `"zh-CN"`,
-      ...namePath.map((word) => `"${word}"`),
-    ];
+    const tempNamePath = [lang === "en" ? `"en-US"` : `"zh-CN"`, ...namePath.map((word) => `"${word}"`)];
     customI18nPath = path.resolve(path.dirname(fileName), customI18nPath);
     const customI18nStr = fs.readFileSync(customI18nPath, "utf-8") as string; // 文件文本
     const targetPosition = getParamPosition(customI18nStr, tempNamePath);
@@ -166,27 +146,19 @@ function jumpI18n(
   }
 
   // 无特别注入的翻译，则从全局路径中找
-  let globalI18nFilePath = path.resolve(
-    fileName.split("src")[0],
-    "src",
-    "locales",
-    lang === "en" ? "en-US" : "zh-CN",
-    namePath.shift() + ".ts"
-  );
-  if(!fs.existsSync(globalI18nFilePath)){
+  let globalI18nFilePath = path.resolve(fileName.split("src")[0], "src", "locales", lang === "en" ? "en-US" : "zh-CN", namePath.shift() + ".ts");
+  if (!fs.existsSync(globalI18nFilePath)) {
     window.showInformationMessage("未找到对应翻译，选区是否正确");
     return;
   }
   const globalI18nStr = fs.readFileSync(globalI18nFilePath, "utf-8") as string; // 文件文本
   const targetPosition = getParamPosition(globalI18nStr, namePath);
   if (targetPosition) {
-    workspace
-      .openTextDocument(Uri.file(globalI18nFilePath))
-      .then((document) => {
-        window.showTextDocument(document, {
-          selection: new Range(targetPosition, targetPosition),
-        });
+    workspace.openTextDocument(Uri.file(globalI18nFilePath)).then((document) => {
+      window.showTextDocument(document, {
+        selection: new Range(targetPosition, targetPosition),
       });
+    });
     return;
   } else {
     window.showInformationMessage("未找到对应翻译，选区是否正确");
@@ -195,15 +167,99 @@ function jumpI18n(
 }
 
 // 跳转至 store 定义处
-function jumpStore(
-  textEditor: TextEditor,
-  edit: TextEditorEdit
-): any {
+function jumpStore(textEditor: TextEditor, edit: TextEditorEdit): any {
   const { document } = textEditor;
   const fileName = document.fileName; // 当前文件完整路径
-  const fileStr = fs.readFileSync(fileName, "utf-8") as string; // 文件文本
-  const lineStr = document.lineAt(textEditor.selection.start.line); // 当前光标所在行字符串
-  const selectionStr = document.getText(textEditor.selection); // 当前选中文本
+  const fileLines = fs.readFileSync(fileName, "utf-8").split("\n"); // 文件文本
+  const lineNum = textEditor.selection.start.line; // 所在行数
+  const lineStr = document.lineAt(lineNum).text; // 当前光标所在行字符串
+  const targetKey = document.getText(textEditor.selection); // 当前选中文本
+  const isMapField = !lineStr.includes("@"); // 有 @ 前缀，则证明是非 MapField，可能是 getter, mutation, action
+  let channel = ""; // 所属渠道
+  let namespace: "root" | "ad" | "adset" | "campaign" = "ad";
+
+  // 根据路径找到当前渠道
+  const channelNames = ["applovin", "facebook", "gdt", "google", "kuaishou", "mintegral", "tiktok", "multi", "toutiao", "unity"];
+  for (let channelIndex = 0; channelIndex < channelNames.length; channelIndex++) {
+    const channelName = channelNames[channelIndex];
+    if (fileName.includes(channelName)) {
+      channel = channelName;
+      break;
+    }
+  }
+
+  // 寻找对应 store 中的那个文件
+  if (isMapField) {
+    let currentLineNum = lineNum;
+    while (currentLineNum >= 0) {
+      if (fileLines[currentLineNum].includes("{")) {
+        if (!fileLines[currentLineNum].includes("mapFields")) {
+          currentLineNum = -1;
+        } else {
+          namespace = (fileLines[currentLineNum].split("/") || "root`")[1].split("'")[0].split("`")[0];
+        }
+        break;
+      }
+      currentLineNum--;
+    }
+    if (currentLineNum < 0) {
+      window.showInformationMessage("未找到对应 store，请选中 mapFields，Getter，mutation 或 action");
+      return;
+    }
+  } else {
+    const namespaceStr = lineStr.split(".")[0];
+    if (namespaceStr.includes("adCreate")) {
+      namespace = "root";
+    } else if (namespaceStr.includes("adset")) {
+      namespace = "adset";
+    } else if (namespaceStr.includes("campaign")) {
+      namespace = "campaign";
+    }
+  }
+
+  // 从文件中搜索关键字
+  function searchKey(searchStr: string, targetFilePaths: string[]) {
+    for (let pathIndex = 0; pathIndex < targetFilePaths.length; pathIndex++) {
+      const targetFilePath = targetFilePaths[pathIndex];
+      console.log(targetFilePath);
+      if (!fs.existsSync(targetFilePath)) {
+        console.log(targetFilePath, "not exist");
+        continue;
+      }
+      const targetFileLines = fs.readFileSync(targetFilePath, "utf-8").split("\n"); // 文件文本
+      let currentLine = 0;
+      while (currentLine < targetFileLines.length) {
+        if (targetFileLines[currentLine].includes(searchStr)) {
+          workspace.openTextDocument(Uri.file(targetFilePath)).then((document) => {
+            const targetPosition = new Position(currentLine, targetFileLines[currentLine].indexOf(searchStr));
+            window.showTextDocument(document, { selection: new Range(targetPosition, targetPosition) });
+          });
+          return true;
+        }
+        currentLine++;
+      }
+    }
+    window.showInformationMessage("未找到对应 store，请选中 mapFields，Getter，mutation 或 action");
+    return false;
+  }
+
+  // 如果是 root 上的 store，则直接找 StoreDimensionModule.ts 和 StoreModuleMulti.ts
+  if (namespace === "root") {
+    const adsCreateStoreDir = path.resolve(fileName.split("src")[0], "src", "views", "adsCreate", "MixinsAdsCreate");
+    if (isMapField) {
+      return searchKey(`${targetKey}:`, [path.resolve(adsCreateStoreDir, "StoreModuleMulti.ts")]);
+    } else {
+      return searchKey(`${targetKey}(`, [path.resolve(adsCreateStoreDir, "StoreModuleMulti.ts"), path.resolve(adsCreateStoreDir, "StoreDimensionModule.ts")]);
+    }
+  }
+
+  // 处理各渠道的 store
+  const channelStoreDir = path.resolve(fileName.split("src")[0], "src", "store", "adsCreate", channel, namespace); // store 文件夹
+  if (isMapField) {
+    return searchKey(`${targetKey}:`, [path.resolve(channelStoreDir, "helper.ts"), path.resolve(channelStoreDir, "index.ts")]);
+  } else {
+    return searchKey(`${targetKey}(`, [path.resolve(channelStoreDir, "index.ts"), path.resolve(channelStoreDir, "helper.ts")]);
+  }
 }
 
 // 搜索使用该翻译的地方
@@ -240,14 +296,12 @@ export function activate(context: ExtensionContext) {
 
   // 设置单词分隔
   languages.setLanguageConfiguration("typescript", {
-    wordPattern:
-      /([^\`\~\!\@\$\^\&\*\(\)\=\+\[\{\]\}\\\|\;\:\'\"\,\.\<\>\/\s]+)/g,
+    wordPattern: /([^\`\~\!\@\$\^\&\*\(\)\=\+\[\{\]\}\\\|\;\:\'\"\,\.\<\>\/\s]+)/g,
   });
 
   // 设置单词分隔
   languages.setLanguageConfiguration("json", {
-    wordPattern:
-      /([^\`\~\!\@\$\^\&\*\(\)\=\+\[\{\]\}\\\|\;\:\'\"\,\.\<\>\/\s]+)/g,
+    wordPattern: /([^\`\~\!\@\$\^\&\*\(\)\=\+\[\{\]\}\\\|\;\:\'\"\,\.\<\>\/\s]+)/g,
   });
 
   context.subscriptions.push(
@@ -262,25 +316,13 @@ export function activate(context: ExtensionContext) {
   );
 
   context.subscriptions.push(
-    commands.registerTextEditorCommand(
-      "i18n-jump.jump-i18n-cn",
-      (textEditor: TextEditor, edit: TextEditorEdit) =>
-      jumpI18n("cn", textEditor, edit)
-    )
+    commands.registerTextEditorCommand("i18n-jump.jump-i18n-cn", (textEditor: TextEditor, edit: TextEditorEdit) => jumpI18n("cn", textEditor, edit))
   );
   context.subscriptions.push(
-    commands.registerTextEditorCommand(
-      "i18n-jump.jump-i18n-en",
-      (textEditor: TextEditor, edit: TextEditorEdit) =>
-      jumpI18n("en", textEditor, edit)
-    )
+    commands.registerTextEditorCommand("i18n-jump.jump-i18n-en", (textEditor: TextEditor, edit: TextEditorEdit) => jumpI18n("en", textEditor, edit))
   );
-  context.subscriptions.push(
-    commands.registerTextEditorCommand("i18n-jump.jump-store", jumpStore)
-  );
-  context.subscriptions.push(
-    commands.registerTextEditorCommand("i18n-jump.search-i18n", searchI18n)
-  );
+  context.subscriptions.push(commands.registerTextEditorCommand("i18n-jump.jump-store", jumpStore));
+  context.subscriptions.push(commands.registerTextEditorCommand("i18n-jump.search-i18n", searchI18n));
 }
 
 export function deactivate() {}
--- todo.md
+++ todo.md
@@ -1,11 +1,5 @@
 <!-- 待完成 -->
-- edit 跳转到
-  - 寻找 helper 是否有值，没有则调整到 index，跳转到第一个 key
-  - adCreateStore 跳转到 StoreModuleMulti
-- action，mutation 跳转
-  - 匹配 ad,adset,campaign 找到对应文件，adCreateStore 
-  - 从 @adCreateStore 中找到赋值语句，解析具体的路径
-  - 是否找到对应的 function，没找到则去父类 StoreDimensionModule 中找
+
 
 
 <!-- 已完成 -->
@@ -13,3 +7,4 @@
 - 在单翻译文件中，点击 key，定位切换中英文 key，
 - 在多翻译文件中，点击 key，定位切换中英文 key，
 - 反推，从具体文案，定位到代码，自动拼接字符串，输入到搜索栏中
+- store state，getter，mutation，action 跳转，跳转到具体 store 定义语句
